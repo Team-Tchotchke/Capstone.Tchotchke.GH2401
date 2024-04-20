fdd407a4891ab4d247204c97a82b5ad03797f751
kiralom <kira.lom@gmail.com>
2024-04-19 20:44:30 -0400

    delete files

--- server/db/migrations/20240327162014_20240327_122000_create_order_table_and_users/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Orders" (
-    "id" SERIAL NOT NULL,
-    "purchase_total" INTEGER NOT NULL,
-    "purchase_date" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Orders_pkey" PRIMARY KEY ("id")
-);
